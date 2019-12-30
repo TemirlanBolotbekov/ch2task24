28edae39fa4c6cc468277a0eb228e99cd90dc338
temirlan <bolotbekov2288@gmail.com>
2019-12-30 09:45:30 +0600

    kuku

---
+++ task24.py
@@ -0,0 +1,13 @@
+#Напишите функцию которая будет определять полиндром ли введенная строка. Если да 2
+#то печатать “True”, если нет “False”.
+def pol(a):
+    return a
+a = (input("Input word :"))
+if a == a[::-1]:
+    print("Polindrom")
+else:
+    print("Ne polindrom")
+()
+
+
+
